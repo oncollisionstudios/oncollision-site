573046a87a624db5f79215ebdd7745056b4e91fb
OnCollision Studio <oncollisionstudios@gmail.com>
2026-05-24 09:06:42 -0700

    E:/Program Files/Git/Git/

--- components/FadeIn.tsx
+++ components/FadeIn.tsx
@@ -9,22 +9,10 @@ export default function FadeIn({
 }) {
   return (
     <motion.div
-      initial={{
-        opacity: 0
-      }}
-
-      whileInView={{
-        opacity: 1
-      }}
-
-      viewport={{
-        once: false,
-        amount: 0.35
-      }}
-
-      transition={{
-        duration: 0.6
-      }}
+      initial={{ opacity: 0, y: 30 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, margin: "-100px" }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
     >
       {children}
     </motion.div>

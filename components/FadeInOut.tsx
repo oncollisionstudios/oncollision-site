15ef02123c6ba61fff94fe664ff63d0c60343048
OnCollision Studio <oncollisionstudios@gmail.com>
2026-05-24 10:07:21 -0700

    Update FadeInOut.tsx

--- components/FadeInOut.tsx
+++ components/FadeInOut.tsx
@@ -20,7 +20,7 @@ export default function FadeInOut({ children }: Props) {
   const opacity = useTransform(
     scrollYProgress,
     [0, 0.25, 0.75, 1],
-    [1, 0.5, 0.25, 0]
+    [0, 1, 1, 0]
   );
 
   return (

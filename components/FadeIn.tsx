872ecdee5d374fba21dc2731df2a0559452cddd2
OnCollision Studio <oncollisionstudios@gmail.com>
2026-05-24 09:02:40 -0700

    .

--- components/FadeIn.tsx
+++ components/FadeIn.tsx
@@ -1,31 +1,30 @@
 "use client";
 
-import { motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
+import { motion } from "framer-motion";
 
 export default function FadeIn({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const ref = useRef(null);
-
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start end", "end start"]
-  });
-
-  // Fade in while entering, fade out while leaving
-  const opacity = useTransform(
-    scrollYProgress,
-    [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
-  );
-
   return (
     <motion.div
-      ref={ref}
-      style={{ opacity }}
+      initial={{
+        opacity: 0
+      }}
+
+      whileInView={{
+        opacity: 1
+      }}
+
+      viewport={{
+        once: false,
+        amount: 0.35
+      }}
+
+      transition={{
+        duration: 0.6
+      }}
     >
       {children}
     </motion.div>
